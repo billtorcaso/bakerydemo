9518ac60b9e872ea110a8534cfd61df90857465b
Bill Torcaso <bill@billtorcaso.org>
2020-02-09 15:01:59 -0500

    Change requirement packages list to include django-extensions and ipython.  I like them because I like 'shell_plus'.

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'bakerydemo.locations',
     'bakerydemo.search',
 
+    'django_extensions',  # Bill's favorite; not needed
+
     'wagtail.contrib.search_promotions',
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
